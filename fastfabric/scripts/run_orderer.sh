8f8606375749571ac1cb220069c7698bfb633330
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2020-05-28 17:37:52 -0400

    use binaries from make command instead of installing binaries in GOBIN

--- fastfabric/scripts/run_orderer.sh
+++ fastfabric/scripts/run_orderer.sh
@@ -4,5 +4,5 @@ source base_parameters.sh
 export FABRIC_LOGGING_SPEC=INFO
 export ORDERER_GENERAL_LOCALMSPDIR=${FABRIC_CFG_PATH}/crypto-config/ordererOrganizations/${ORDERER_DOMAIN}/orderers/${ORDERER_ADDRESS}.${ORDERER_DOMAIN}/msp
 
-(cd ${FABRIC_ROOT}/orderer/ && go install)
+(cd ${FABRIC_ROOT} && make orderer)
 orderer start
